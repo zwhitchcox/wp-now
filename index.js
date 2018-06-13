c3969d8045130c4ebb7d3aeccbb95886efaa4ae5
Zane Hitchcox <zwhitchcox@gmail.com>
2018-06-13 10:58:29 -0400

    add versioned installation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cp = require('child_process')
 const fs = require('fs')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const WebpackSourceMapSupport = require('webpack-source-map-support')
+const NpmInstallPlugin = require('npm-install-webpack-plugin')
 const path = require('path')
 const rimraf = require('rimraf')
 const project_root = process.cwd()
@@ -20,63 +21,52 @@ const server_path = check_args(['-s', '--server']) ||  project_root + '/server.j
 const client_path = check_args(['-c', '--client']) ||  project_root + '/client.js'
 const install_auto = check_args(['-a', '--auto-install']) ||  project_root + '/client.js'
 const package = require(project_root + '/package.json')
-
-const still_required = [
-  'babel-polyfill',
-  'source-map-support',
-  ['babel-loader', '^8.0.0-beta'],
-  '@babel/core',
-  '@babel/runtime',
-  '@babel/plugin-proposal-decorators',
-].filter(dep =>  {
-  if (Array.isArray(dep))
-    dep = dep[0]
-    return !(dep in (package.dependencies || {}) || dep in (package.devDependencies || {}))
-}).map(dep => Array.isArray(dep) && typeof dep[1] === 'string' ? dep[0] + '@' + dep[1] : dep)
-
 ;(async () => {
-let l;
-if (l = still_required.length) {
-  const install = readline_sync.question("We've detected that you still required the dependencies " + still_required.slice(0, l - 2).join(", ") + (l > 1 ? "and " : "") + still_required[l - 1] + " would you like to install them now? y/n\n")
-  if (install[0] === 'y' || check_args_bool(['--yes', '-y'])) {
-    const args = [using_yarn() ? 'add' : 'i'].concat(still_required)
-    await new Promise((rej, res) => {
-      const proc = cp.spawn(using_yarn() ? 'yarn' : 'npm', args, {
-        stdio: 'inherit',
-        cwd: project_root,
-      })
-      proc.on('exit', res)
-    }).catch(console.error)
+  
+  await try_install([
+    '@babel/core',
+    'babel-polyfill',
+    'css-loader',
+    'url-loader',
+    'react-svg-loader',
+    'source-map-support',
+    ['babel-loader', '^8.0.0-beta'],
+    '@babel/runtime',
+    '@babel/preset-react',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-decorators',
+  ]
+    .filter(check_dep)
+    .map(dep => Array.isArray(dep) && typeof dep[1] === 'string' ? dep[0] + '@' + dep[1] : dep)
+  )
+
+  const babelrc_path = project_root + '/.babelrc'
+
+  const client_exists = fs.existsSync(client_path)
+  const server_exists = fs.existsSync(server_path)
+  const babelrc_exists = fs.existsSync(babelrc_path)
+
+  function Clean() {}
+  Clean.prototype.apply = function(compiler) {
+    compiler.hooks.compile.tap('clean-webpack-plugin', () => rimraf.sync(project_root + '/build/**'))
   }
-}
-const babelrc_path = project_root + '/.babelrc'
+  let babel_opts = {
+    presets: [
+      babel_preset_env,
+      babel_preset_react,
+    ],
+    plugins: [
+      babel_plugin_transform_runtime,
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true}],
+    ],
+  }
+  if (babelrc_exists) {
+    const babelrc = fs.readFileSync(babelrc_path)
+    babel_opts = JSON.parse(babelrc)
+  }
 
-const client_exists = fs.existsSync(client_path)
-const server_exists = fs.existsSync(server_path)
-const babelrc_exists = fs.existsSync(babelrc_path)
-
-function Clean() {}
-Clean.prototype.apply = function(compiler) {
-  compiler.hooks.compile.tap('clean-webpack-plugin', () => rimraf.sync(project_root + '/build/**'))
-}
-let babel_opts = {
-  presets: [
-    babel_preset_env,
-    babel_preset_react,
-  ],
-  plugins: [
-    babel_plugin_transform_runtime,
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-  ],
-}
-if (babelrc_exists) {
-  const babelrc = fs.readFileSync(babelrc_path)
-  babel_opts = JSON.parse(babelrc)
-}
-
-const config = []
-if (client_exists) 
-  config.push({
+  const client_config = {
     entry: [
       'babel-polyfill',
       client_path,
@@ -87,7 +77,7 @@ if (client_exists)
           test: /\.svg$/,
           use: [
             {
-              loader: require("babel-loader")
+              loader: "babel-loader"
             },
             {
               loader: "react-svg-loader",
@@ -135,6 +125,12 @@ if (client_exists)
       filename: 'client.js'
     },
     plugins: [
+      new NpmInstallPlugin({
+        dev: false,
+        peerDependencies: true,
+        quiet: false,
+        yarn: true,
+      }),
       new MiniCssExtractPlugin({
         filename: "main.css",
         chunkFilesname: "[id].css",
@@ -150,10 +146,9 @@ if (client_exists)
     context: project_root,
     devtool: 'eval-source-map',
     mode: 'development',
-  })
+  }
 
-if (server_exists) 
-  config.push({
+  const server_config = {
     entry: [
       'babel-polyfill',
       server_path,
@@ -185,6 +180,12 @@ if (server_exists)
       filename: 'server.js'
     },
     plugins: [
+      new NpmInstallPlugin({
+        dev: false,
+        peerDependencies: true,
+        quiet: false,
+        yarn: true,
+      }),
       new Clean,
       new webpack.DefinePlugin({
         'process.env.BROWSER': false,
@@ -200,43 +201,57 @@ if (server_exists)
       __dirname: false,
     },
     stats: 'minimal',
-  })
-
-if (!config.length) {
-  console.log('neither client, nor server config were found')
-}
-
-let server;
-
-function start_server() {
-  kill_server()
-  server = cp.spawn('node', [project_root + '/build/server'], {stdio: 'inherit', cwd: project_root})
-}
-
-process.on('exit', kill_server)
-
-function kill_server() {
-  if(server) {
-    server.kill('SIGTERM')
   }
-}
 
-let server_process;
-config.forEach(conf => {
-  webpack(conf).watch({
-    aggregateTimeout: 300,
-    poll: undefined
-  }, (err, stats) => {
-    if (server_exists) start_server()
-    if (err || stats.hasErrors()) {
-      stats.toJson()
-    } else {
-      stats.toJson('minimal')
+  if (!(server_exists || client_exists)) {
+    throw new Error('neither client, nor server files were found')
+  }
+
+  let server;
+
+  function start_server() {
+    kill_server()
+    server = cp.spawn('node', [project_root + '/build/server'], {stdio: 'inherit', cwd: project_root})
+  }
+
+  process.on('exit', kill_server)
+
+  function kill_server() {
+    if(server) {
+      server.kill('SIGTERM')
     }
-  })
-})
+  }
+
+  let server_process;
+  if (server_exists) {
+    webpack(server_config).watch({
+      aggregateTimeout: 300,
+      poll: undefined
+    }, (err, stats) => {
+      if (server_exists) start_server()
+      if (err || stats.hasErrors()) {
+        stats && stats.toJson()
+      } else {
+        stats.toJson('minimal')
+      }
+    })
+  }
+  if (client_exists) {
+    webpack(client_config).watch({
+      aggregateTimeout: 300,
+      poll: undefined
+    }, (err, stats) => {
+      console.log("client built")
+      if (err || stats.hasErrors()) {
+        stats && stats.toJson()
+      } else {
+        stats.toJson('minimal')
+      }
+    })
+  }
   
 })()
+
 
 
 
@@ -259,9 +274,41 @@ function check_args_bool(check_arg) {
   const args = process.argv.slice(2)
   return args.some(arg => check_arg.includes(arg))
 }
+
 function using_yarn() {
   if (fs.existsSync(project_root + '/yarn.lock')) return true
 }
 
 
+async function try_install(deps) {
+  console.log("deps = ", deps)
+  if (l = deps.length) {
+    const args = [using_yarn() ? 'add' : 'i'].concat([using_yarn() ? '--dev' : '--save-dev']).concat(deps)
+    await new Promise((rej, res) => {
+      const proc = cp.spawn(using_yarn() ? 'yarn' : 'npm', args, {
+        stdio: 'inherit',
+        cwd: project_root,
+      })
+      proc.on('exit', () => console.log('done installing') || res())
+    }).catch(console.error)
+  }
+}
 
+function check_dep(dep) {
+  if (Array.isArray(dep)) {
+    const version = dep[1]
+    dep = dep[0]
+    if (dep in (package.dependencies || {})) {
+      const cur_major      = +(/\d/.exec(package.dependencies[dep])[0])
+      const required_major = +(/\d/.exec(version)[0])
+      return cur_major <= required_major
+    } else if (dep in (package.devDependencies || {})) {
+      const cur_major      = +(/\d/.exec(package.devDependencies[dep])[0])
+      const required_major = +(/\d/.exec(version)[0])
+      return cur_major <= required_major
+    } else {
+      return true 
+    }
+  }
+    else return !(dep in (package.dependencies || {}) || dep in (package.devDependencies || {}))
+}
